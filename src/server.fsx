a4a4a08ffdc3e5afbca6764ff69f6fad3074f491
dominikus1993 <dominikus1910@gmail.com>
2016-08-19 17:29:24 +0200

    add simple routing to suave file

---
+++ src/server.fsx
@@ -0,0 +1,17 @@
+#r "../packages/Suave/lib/net40/Suave.dll"
+
+open Suave
+open Suave.Filters
+open Suave.Successful
+open Suave.Operators
+open Suave.Files
+open Suave.Sockets
+open Suave.Sockets.Control
+open Suave.WebSocket
+
+let app : WebPart =
+    choose [ GET >=> choose [ path "/" >=> file "index.html"
+                              browseHome ]
+             RequestErrors.NOT_FOUND "Found no handlers." ]    
+
+
