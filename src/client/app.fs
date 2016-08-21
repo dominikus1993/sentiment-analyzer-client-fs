b2f36b07b2b20593e84e5cfb1c262503f027b48a
dominikus1993 <dominikus1910@gmail.com>
2016-08-21 18:18:59 +0200

    add component to app

--- src/client/app.fs
+++ src/client/app.fs
@@ -7,8 +7,12 @@ open Fable.Import
 module R = Fable.Helpers.React
 module P = Fable.Helpers.React.Props
 
+let handler query = 
+    printf "%A" query
+
 type App(props) = 
     inherit React.Component<obj,obj>(props)
 
     member this.render () = 
-        R.div [P.ClassName "app"] [R.h1[] [R.p[] [unbox "Dzien dobry"]]]
+        let form = R.com<SearchBox.SearchBox,_,_> {Handler = handler} []
+        R.div [P.ClassName "app"] [form]
