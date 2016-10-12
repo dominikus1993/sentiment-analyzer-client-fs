4925574086e573c36f609dd3f82bc47b1fd883b1
dominikus1993 <dominikus1910@gmail.com>
2016-10-12 18:04:49 +0200

    add HandleSubmit method

--- src/client/tweetTextInput.fs
+++ src/client/tweetTextInput.fs
@@ -1,6 +1,7 @@
 module TweetTextInput
 open System
 open Fable.Core
+open Fable.Core.JsInterop
 open Fable.Import
 module React = Fable.Import.React
 module R = Fable.Helpers.React
@@ -18,6 +19,12 @@ type TweetTextInput(props, ctx) as this =
     inherit React.Component<ITweetTextInputProps, TweetTextInputState>(props, ctx)
     do this.state <- { Text = defaultArg this.props.Text "" }
 
+    member this.HandleSubmit(e: React.KeyboardEvent) =
+        if e.which = Dto.ENTER_KEY then
+            let text = (unbox<string> e.target?value).Trim()
+            this.props.OnSave(text)
+            if this.props.Search then
+                this.setState({ Text = "" })
     member this.render() =
         R.h1 [] [unbox "Hello world"]
         
