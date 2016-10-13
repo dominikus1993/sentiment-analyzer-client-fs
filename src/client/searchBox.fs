314178baab15043d52e2149d8a27d42efa0f9ed8
dominikus1993 <dominikus1910@gmail.com>
2016-10-13 19:30:29 +0200

    add button default

--- src/client/searchBox.fs
+++ src/client/searchBox.fs
@@ -1,5 +1,6 @@
 [<RequireQualifiedAccess>]
 module SearchBox
+open Dto
 open System
 open TweetTextInput
 open Fable.Core
@@ -21,12 +22,13 @@ type SearchBoxComponent(props) as this =
                         if text.Length <> 0 then
                             this.props.Search text
                     member __.Text = None
-                    member __.Placeholder = "Wpisz szukaną fraze"
+                    member __.Placeholder = "Wpisz fraze"
                     member __.Search = true
                 } []
         let form = R.div[P.ClassName "form-group"] [
                         textInput
                         R.input[
+                            P.ClassName (classNames [("btn", true); ("btn-default", true); ("btn-lg", true)])
                             P.Type "submit"
                             P.Value (U2.Case1 "Post")
                             ][]
