2e5099ee8c58c5cb3be3b4e32e870c8d61da7497
dominikus1993 <dominikus1910@gmail.com>
2016-10-25 21:19:17 +0200

    add trend component

---
+++ src/client/trend.fs
@@ -0,0 +1,15 @@
+module Trend
+open System
+open Fable.Core
+open Fable.Core.JsInterop
+open Dto
+open Utils
+module React = Fable.Import.React
+module R = Fable.Helpers.React
+module P = Fable.Helpers.React.Props
+
+type TrendComponent(props) =
+    inherit React.Component<Tweets, obj>(props)
+
+    member x.render () =
+        R.div [ P.ClassName "score" ] []
