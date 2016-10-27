ceca1bcea7810a9fd677c5a46c1ed1d2eb746584
dominikus1993 <dominik_kotecki@rossmann.com.pl>
2016-10-27 10:32:41 +0200

    small changes

--- src/client/trend.fs
+++ src/client/trend.fs
@@ -8,8 +8,12 @@ module React = Fable.Import.React
 module R = Fable.Helpers.React
 module P = Fable.Helpers.React.Props
 
+module TrendUtils =
+    let assignTrend(tweets: Tweets) =   
+        2
+
 type TrendComponent(props) =
     inherit React.Component<Tweets, obj>(props)
-
     member x.render () =
         R.div [ P.ClassName "score" ] []
+        
